98046ed9aa4f786f4e159987fc4da6f7fcaeca49
Angel Alagao <angelalagao@gmail.com>
2017-05-31 15:36:16 -0400

    added pseudocode

--- dev/scripts/script.js
+++ dev/scripts/script.js
@@ -1,3 +1,17 @@
+// User inputs start location and destination (address format)
+// confirm origin and destination are both valid locations
+// On submit, obtain address value and store in variables
+// Convert user values to latitude and longitude using google geocodes
+// Store user lat long in an array within an object 
+// Get data from city bikes api and for each result, map through that array to return a new array of objects each with the lat and long for each station
+// Create a function using google's computeDistanceBetween() function enter user and citybikes latlong objects as arguments which will calculate the distance in km
+// Apply the function to both starting location and destination
+// sort from shortest distance to longest
+// take the results with the shortest distance 
+// get directions from starting station to end station with distance matrix API
+// push to DOM duration, distance and markers onto the map
+
+
 const bikeApp = {};
 bikeApp.cityBikesUrl = 'http://api.citybik.es/v2/networks';
 bikeApp.cityBikesToronto = 'https://tor.publicbikesystem.net/ube/gbfs/v1/';
